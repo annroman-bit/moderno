3af1fbbef575c782c80c98d915c70d9e8a85aad4
annroman-bit <annroman.bit@gmail.com>
2020-02-12 13:47:15 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,20 @@ $(function(){
 
   var mixer = mixitup('.products__inner-box');
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.product-slider__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true,
+    arrows: false
+
+
+  });
+
 });
