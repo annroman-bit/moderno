a1b15bd75ac1b452cb22e6485d479e249a6444a9
annroman-bit <annroman.bit@gmail.com>
2020-02-27 16:19:10 +0200

    added adaptiv part two

--- app/js/main.js
+++ app/js/main.js
@@ -11,7 +11,33 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
-    arrows: false
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 1441,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,          
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,          
+        }
+      },
+    ]
+   
   });
 
   $(".js-range-slider").ionRangeSlider({
